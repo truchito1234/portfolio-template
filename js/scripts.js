c7c07742feb265e24c5aa4e655f851fe357af4e5
Enzo Aramayo <nelsonangramainyu@gmail.com>
2022-07-09 09:01:19 -0300

    Se agrega sección tecnologías

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+(function($) {
+    "use strict";
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate({
+                        scrollTop: target.offset().top,
+                    },
+                    100,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+    $(".js-scroll-trigger").click(function() {
+        $(".navbar-collapse").collapse("hide");
+    });
+})(jQuery);
